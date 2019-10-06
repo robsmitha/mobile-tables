509116a3a5402e33dc0554e50888ed71930e8800
robsmitha <robsmitha94@gmail.com>
2019-10-05 22:42:24 -0400

    Initial Commit

---
+++ mobile-tables.js
@@ -0,0 +1,34 @@
+
+
+$.fn.MobileTables = function(options) {
+
+    // Default options
+    var settings = $.extend({
+        width: 769
+    }, options );
+ 
+
+    this.each(function() {
+        if(!$(this).hasClass('.mobile-table')){
+            if ($(window).width() < settings.width) {
+                var html = ''
+                $(this).find('tbody tr').each(function () {
+                    html += '<div style="padding: 1rem; background-color: #fff; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem;">'
+                    $(this).find('td').each(function () {
+                        var index = $(this).index()
+                        var label = $(this).closest('table').find('th').eq(index).html()
+                        html += '<div>'
+                        if(label && label.length > 0){
+                            html += '<strong>' + label + ':</strong>&nbsp;'
+                        }
+                        html += $(this).html()
+                        html += '</div>'
+                    })
+                    html += '</div>'
+                })
+                $(this).html(html)
+                $(this).addClass('mobile-table')
+            }
+        }
+    });
+};
